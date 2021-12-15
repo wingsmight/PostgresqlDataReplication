1c1f64412b15e62a06e005ffa28185bcb24ea429
wingsmight <wingsmight@gmail.com>
2021-12-16 00:13:38 +0700

    Initial Commit

---
+++ PostgresqlDataReplication/PostgresqlDataReplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostgresqlDataReplicationApp.swift
+//  PostgresqlDataReplication
+//
+//  Created by Igoryok on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct PostgresqlDataReplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
